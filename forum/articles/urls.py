f4ca20e52e02c6360cd2ef9277b71b95a9573444
zhuf <zhufeng314@163.com>
2018-04-11 22:29:47 +0800

    增加发表文章页面

--- forum/articles/urls.py
+++ forum/articles/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^list/(?P<block_id>\d+)', views.article_list)
+    url(r'^list/(?P<block_id>\d+)', views.article_list),
+    url(r'^create/(?P<block_id>\d+)', views.create_article)
 ]
