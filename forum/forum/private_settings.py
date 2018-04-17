0772fa64dbeda7bf73744293d22b37f331c11970
zhuf <zhufeng314@163.com>
2018-04-17 16:38:25 +0800

    邮箱设置

---
+++ forum/forum/private_settings.py
@@ -0,0 +1,6 @@
+EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.163.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'xxmeteobservatory@163.com'
+EMAIL_HOST_PASSWORD = 'e54d8a10b6b'
+DEFAULT_FROM_EMAIL = 'xxmeteobservatory@163.com'
--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+# 保密设置
+from . import private_settings
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
