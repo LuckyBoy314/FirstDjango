bad7f6ea2f244668923593a7a708060192e833c5
ZHUF <zhufeng314@163.com>
2018-05-25 09:57:38 +0800

    Django模板

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Board
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html',{'boards': boards})
+
+def board_topics(request, pk):
+    # try:
+    #     board = Board.objects.get(pk=pk)
+    # except Board.DoesNotExist:
+    #     raise Http404
+    board = get_object_or_404(Board, pk=pk) # 更简洁的方式
+    return render(request, 'topics.html', {'board': board})
