2e6b617850b8cf1ddb904bc9492902bee20cbcb7
ZHUF <zhufeng314@163.com>
2018-06-01 15:15:22 +0800

    模板处理

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Board
 
+
+# 目前view的主要作用是从数据库中查询提取数据
+# 但如何创建数据库还是个问题
+# 创建数据的方法似乎只有手工或者由表单创建两种方式
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html',{'boards': boards})
