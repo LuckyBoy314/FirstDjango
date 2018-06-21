a0675091e825f171b7468213530c02cd91fb01cc
ZHUF <zhufeng314@163.com>
2018-06-21 08:38:07 +0800

    分页功能

--- forum2/article/views.py
+++ forum2/article/views.py
@@ -5,18 +5,52 @@ from django.urls import reverse
 from .forms import ArticleForm
 from django.views.generic import View
 from django.views.generic import DetailView
+from django.core.paginator import Paginator
 
+# def article_list(request, block_id):
+#     block = Block.objects.get(id=block_id)
+#
+#     # 两种方法获取articles
+#     # 方法一：正常获取
+#     articles = Article.objects.filter(block=block, status=0)
+#
+#     # 方法二：也可以使用反向映射关系获取
+#     articles = block.article_set.all().filter(status=0)
+#     return render(request, 'article_list.html', {'b': block, 'articles': articles})
 
+# 加入分页功能
 def article_list(request, block_id):
+    block_id = int(block_id)
     block = Block.objects.get(id=block_id)
 
-    # 两种方法获取articles
-    # 方法一：正常获取
-    articles = Article.objects.filter(block=block, status=0)
+    ARTICLE_CNT_1PAGE = 1
+    page_no = int(request.GET.get('page_no', 1))  # 当前页码
 
-    # 方法二：也可以使用反向映射关系获取
-    articles = block.article_set.all().filter(status=0)
-    return render(request, 'article_list.html', {'b': block, 'articles': articles})
+    # 手工实现分页
+    # start_index = (page_no-1)*ARTICLE_CNT_1PAGE
+    # end_index = page_no*ARTICLE_CNT_1PAGE
+    # articles = Article.objects.filter(block=block, status=0).order_by('-id')[start_index:end_index]
+
+    # 使用Paginator实现分页
+    all_articles = Article.objects.filter(block=block, status=0).order_by('-id')
+    p = Paginator(all_articles, ARTICLE_CNT_1PAGE)  # 构建分页器,只需要所有对象和每页对象数量两个变量
+    page = p.page(page_no)  # 当前页面
+    articles = page.object_list  # 当前页中的所有对象
+
+    # 分页控件参数
+    pages_cnt = p.num_pages  # 总页数
+    current_no = page_no  # 当前页码
+    page_links = [i for i in range(page_no - 2, page_no + 3) if i > 0 and i <= pages_cnt]
+    previous_link = page_links[0] - 1
+    next_link = page_links[-1] + 1
+    has_previous = previous_link > 0  # True if previous_link > 0 else False
+    has_next = next_link <= pages_cnt  # True if next_link <= pages_cnt else False
+
+    return render(request, 'article_list.html', {'articles': articles, 'b': block,
+                                                 'pages_cnt': pages_cnt, 'current_no': current_no,
+                                                 'page_links': page_links,
+                                                 'previous_link': previous_link, 'next_link': next_link,
+                                                 'has_previous': has_previous, 'has_next': has_next})
 
 
 # 基于函数的创建文章方法
@@ -95,7 +129,7 @@ def article_detail(request, article_id):
     return render(request, 'article_detail.html', {'a': article})
 
 
-# 基于类(详情类）显示文章的方法，详情类DetailView用于在一个网页中显示一个数据模型实例的详情，这种通用的方法被抽象成了详情类
+# 基于类(视图详情）显示文章的方法，视图详情类DetailView用于在一个网页中显示一个数据模型实例的详情，这种通用的方法被抽象成了详情类
 # 在展示一个数据对象的通用类方法：五个地方需要注意：
 # 1. 类继承于DetailView
 # 2. 从url传入数据对象在数据库中的id，基于函数的是(?P<aid>\d+),aid是函数的参数,在基于类的配置中使用(?P<pk>\d+), <pk>表示primarykey，
